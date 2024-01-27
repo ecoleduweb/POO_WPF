396dd95f5f5d7c06b40a9084b3dd279462b3d45e
Antoine Chagnon Michaud <achagnonmichaud@cvm.qc.ca>
2024-01-27 11:08:51 -0500

    Removed answer from ChiffreSecret

--- ChiffreSecret/MainWindow.xaml.cs
+++ ChiffreSecret/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 
 namespace ChiffreSecret
 {
@@ -12,70 +11,12 @@ namespace ChiffreSecret
         {
             InitializeComponent();
             PrepareButtons();
-            InitGame();
-        }
-
-        private void InitGame()
-        {
-            tbHint.Text= "?";
-            NumberOfAttempts = 0;
-            NumberToFind = Random.Next(1, 20);
+            // Va voir le menu!
         }
 
         private void PrepareButtons()
         {
-            for (int i = 1; i <= 20; i++)
-            {
-                GridButtons.ColumnDefinitions.Add(new ColumnDefinition());
-                var button = new Button()
-                {
-                    Content = i.ToString(),
-                    HorizontalAlignment = HorizontalAlignment.Stretch,
-                };
-                button.Click += OnBtnNumberClick;
-                Grid.SetColumn(button, i-1);
-                GridButtons.Children.Add(button);
-            }
-        }
-
-        void OnBtnNumberClick(object sender, RoutedEventArgs e)
-        {
-            ((Button)sender).IsEnabled = false;
-            if (int.TryParse(((Button)sender).Content.ToString(), out int selectedNumber))
-            {
-                NumberOfAttempts++;
-                if (selectedNumber > NumberToFind)
-                {
-                    tbHint.Text = "↓";
-                }
-                else if (selectedNumber < NumberToFind)
-                {
-                    tbHint.Text = "↑";
-                }
-                else
-                {
-                    MessageBox.Show($"Le nombre était {NumberToFind} \nVous avez trouvé en {NumberOfAttempts} essai{(NumberOfAttempts > 1 ? "s" : "")}");
-                    StartANewGame();
-                }
-            }
-        }
-
-        void OnMenuNewGameClick(object sender, RoutedEventArgs e)
-        {
-            StartANewGame();
-        }
-
-        private void StartANewGame()
-        {
-            MessageBoxResult result = MessageBox.Show("Voulez-vous démarrer une nouvelle partie?", "Nouvelle partie?", MessageBoxButton.YesNo);
-            if (result == MessageBoxResult.Yes)
-            {
-                InitGame();
-            }
-            else
-            {
-                Environment.Exit(0);
-            }
+            // Ici tu vas préparer tes boutons.
         }
     }
 }
